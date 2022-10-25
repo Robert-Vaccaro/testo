8ee935cc45eb21342d40b262486f388ee3074def
robert-vaccaro <robertvaccarotech@gmail.com>
2022-10-25 16:15:41 -0700

    tsting

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+const apiKey = process.env.REACT_APP_ACCESS_KEY
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {apiKey}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
